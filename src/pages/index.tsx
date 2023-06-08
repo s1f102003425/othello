521f8c180ef74a2b948a150f86f54fe7cce541ad
s1f102003425 <s1f102003425@iniad.org>
2023-06-09 06:57:08 +0900

    レベル１達成＋手番表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,8 +13,48 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const directions = [
+    [-1, -1],
+    [-1, 0],
+    [0, -1],
+    [1, 1],
+    [1, 0],
+    [0, 1],
+    [1, -1],
+    [-1, 1],
+  ];
   const onClick = (x: number, y: number) => {
-    if (board[y][x] === 0 && board[y + 1][x] === 3 - turnColor) {
+    const newBoard: number[][] = JSON.parse(JSON.stringify(board));
+    let ok = false;
+    for (const direction of directions) {
+      if (board[y + direction[1]] === undefined) {
+        continue;
+      }
+      if (board[y][x] === 0 && board[y + direction[1]][x + direction[0]] === 3 - turnColor) {
+        console.log(x, y);
+        for (let i = 1; i < 8; i++) {
+          if (board[y + i * direction[1]][x + i * direction[0]] === undefined) {
+            break;
+          } else if (board[y + i * direction[1]][x + i * direction[0]] === 0) {
+            break;
+          } else if (board[y + i * direction[1]][x + i * direction[0]] === 3 - turnColor) {
+            continue;
+          } else if (board[y + i * direction[1]][x + i * direction[0]] === turnColor) {
+            for (let j = i; j > 0; j--) {
+              newBoard[y + j * direction[1]][x + j * direction[0]] = turnColor;
+            }
+            ok = true;
+            break;
+          }
+        }
+      }
+    }
+    if (ok) {
+      newBoard[y][x] = turnColor;
+      setBoard(newBoard);
+      setTurnColor(3 - turnColor);
+    }
+    /*if (board[y][x] === 0 && board[y + 1][x] === 3 - turnColor) {
       console.log(x, y);
       const newBoard: number[][] = JSON.parse(JSON.stringify(board));
       for (let i = 1; i < 8; i++) {
@@ -33,7 +73,7 @@ const Home = () => {
           break;
         }
       }
-    }
+    }*/
     //if (board[y + 1] !== undefined && board[y + 1][x] !== 0 && board[y + 1][x] !== turnColor) {
     //newBoard[y][x] = turnColor;
     //setTurnColor(3 - turnColor);
@@ -56,6 +96,7 @@ const Home = () => {
           ))
         )}
       </div>
+      <p>{turnColor === 1 ? '黒' : '白'}の手盤です</p>
     </div>
   );
 };
