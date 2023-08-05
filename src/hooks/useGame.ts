4d45ebaddbff119fb462816c93bb3202da0fcdde
s1f102003425 <s1f102003425@iniad.org>
2023-08-05 17:39:10 +0900

    候補地作成の箇所を関数か関数化

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -4,10 +4,10 @@ export const useGame = () => {
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, -1, 0, 0, 0, 0],
+    [0, 0, -1, 2, 1, 0, 0, 0],
+    [0, 0, 0, 1, 2, -1, 0, 0],
+    [0, 0, 0, 0, -1, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
@@ -30,54 +30,93 @@ export const useGame = () => {
     });
     for (const direction of directions) {
       if (
-        newBoard[y + direction[1]] !== undefined &&
-        newBoard[y][x] === 0 &&
-        newBoard[y + direction[1]][x + direction[0]] === 3 - turnColor
+        // newBoard[y + direction[1]] !== undefined &&
+        board[y][x] < 1
+        // newBoard[y + direction[1]][x + direction[0]] === 3 - turnColor
       ) {
         console.log(x, y);
         for (let i = 1; i < 8; i++) {
           if (
-            newBoard[y + i * direction[1]] === undefined ||
-            newBoard[y + i * direction[1]][x + i * direction[0]] === undefined ||
-            newBoard[y + i * direction[1]][x + i * direction[0]] === 0
+            board[y + i * direction[1]] === undefined ||
+            board[y + i * direction[1]][x + i * direction[0]] === undefined ||
+            board[y + i * direction[1]][x + i * direction[0]] === 0
           ) {
             break;
-          } else if (newBoard[y + i * direction[1]][x + i * direction[0]] === 3 - turnColor) {
+          } else if (board[y + i * direction[1]][x + i * direction[0]] === 3 - turnColor) {
             continue;
-          } else if (newBoard[y + i * direction[1]][x + i * direction[0]] === turnColor) {
+          } else if (board[y + i * direction[1]][x + i * direction[0]] === turnColor) {
             for (let j = i; j > 0; j--) {
               newBoard[y + j * direction[1]][x + j * direction[0]] = turnColor;
             }
+            newBoard[y][x] = turnColor;
             break;
           }
         }
       }
     }
-    newBoard[y][x] = turnColor;
+
     //  候補地を出す
+    makeCandidate(newBoard,turnColor);
+    // let candidateCount = 0;
+    // for (let subX = 0; subX < 8; subX++) {
+    //   for (let subY = 0; subY < 8; subY++) {
+    //     for (const direction of directions) {
+    //       if (
+    //         newBoard[subY + direction[1]] !== undefined &&
+    //         newBoard[subY][subX] === 0 &&
+    //         newBoard[subY + direction[1]][subX + direction[0]] === turnColor
+    //       ) {
+    //         for (let i = 1; i < 8; i++) {
+    //           if (
+    //             newBoard[subY + i * direction[1]] === undefined ||
+    //             newBoard[subY + i * direction[1]][subX + i * direction[0]] === undefined ||
+    //             newBoard[subY + i * direction[1]][subX + i * direction[0]] === 0
+    //           ) {
+    //             break;
+    //           } else if (newBoard[subY + i * direction[1]][subX + i * direction[0]] === turnColor) {
+    //             continue;
+    //           } else if (
+    //             newBoard[subY + i * direction[1]][subX + i * direction[0]] ===
+    //             3 - turnColor
+    //           ) {
+    //             newBoard[subY][subX] = -1;
+    //             candidateCount++;
+    //             break;
+    //           }
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
+    console.table(newBoard);
+    setBoard(newBoard);
+  };
+  const makeCandidate = (candidateBoard: number[][],turnPlayerColor:number) => {
     let candidateCount = 0;
     for (let subX = 0; subX < 8; subX++) {
       for (let subY = 0; subY < 8; subY++) {
         for (const direction of directions) {
           if (
-            newBoard[subY + direction[1]] !== undefined &&
-            newBoard[subY][subX] === 0 &&
-            newBoard[subY + direction[1]][subX + direction[0]] === turnColor
+            candidateBoard[subY + direction[1]] !== undefined &&
+            candidateBoard[subY][subX] === 0 &&
+            candidateBoard[subY + direction[1]][subX + direction[0]] === turnPlayerColor
           ) {
             for (let i = 1; i < 8; i++) {
               if (
-                newBoard[subY + i * direction[1]] === undefined ||
-                newBoard[subY + i * direction[1]][subX + i * direction[0]] === undefined ||
-                newBoard[subY + i * direction[1]][subX + i * direction[0]] === 0
+                candidateBoard[subY + i * direction[1]] === undefined ||
+                candidateBoard[subY + i * direction[1]][subX + i * direction[0]] === undefined ||
+                candidateBoard[subY + i * direction[1]][subX + i * direction[0]] === 0
               ) {
                 break;
-              } else if (newBoard[subY + i * direction[1]][subX + i * direction[0]] === turnColor) {
+              } else if (
+                candidateBoard[subY + i * direction[1]][subX + i * direction[0]] === turnPlayerColor
+              ) {
                 continue;
               } else if (
-                newBoard[subY + i * direction[1]][subX + i * direction[0]] ===
-                3 - turnColor
+                candidateBoard[subY + i * direction[1]][subX + i * direction[0]] ===
+                3 - turnPlayerColor
               ) {
-                newBoard[subY][subX] = -1;
+                candidateBoard[subY][subX] = -1;
                 candidateCount++;
                 break;
               }
@@ -86,8 +125,6 @@ export const useGame = () => {
         }
       }
     }
-    console.table(newBoard);
-    setBoard(newBoard);
     if (candidateCount !== 0) {
       setTurnColor(3 - turnColor);
     }
